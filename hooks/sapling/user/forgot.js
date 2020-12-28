3997d373956d4ae42c800f3975717beb75affb62
groenroos <oskari@groenroos.fi>
2020-12-28 21:30:32 +0000

    Closes #4

--- hooks/sapling/user/forgot.js
+++ hooks/sapling/user/forgot.js
@@ -14,49 +14,48 @@ const Response = require("../../../lib/Response");
 module.exports = async function(app, req, res) {
 
 	/* Get authkey and identifiable from database */
-	const { authkey, email } = await this.app.storage.get({
+	const { authkey, email } = await app.storage.get({
 		url: `/data/users/email/${req.body.email}/?single=true`,
-		session: App.adminSession
+		session: app.adminSession
 	});
 
-	/* Only allow sending authkey once every 2 hours */
-	/* TODO: Revisit whether this is actually necessary */
-	if (authkey) {
-		var key = parseInt(authkey.substring(0, authkey.length - 11), 16);
-		const diff = key - Date.now();
-
-		if (diff > 0) {
-			const hours = diff / 60 / 60 / 1000;
-			new Response(app, req, res, new SaplingError(`Must wait ${hours.toFixed(1)} hours before sending another recovery email.`));
+	/* Only do stuff if we found a user */
+	if(email) {
+		/* Only allow sending authkey once every 2 hours */
+		/* TODO: Revisit whether this is actually necessary */
+		if (authkey) {
+			var key = parseInt(authkey.substring(0, authkey.length - 11), 16);
+			const diff = key - Date.now();
+	
+			if (diff > 0) {
+				const hours = diff / 60 / 60 / 1000;
+				new Response(app, req, res, new SaplingError(`Must wait ${hours.toFixed(1)} hours before sending another recovery email.`));
+			}
 		}
+	
+		/* Make sure key is > Date.now() */
+		var key = (Date.now() + 2 * 60 * 60 * 1000).toString(16);
+		key += app.utils.randString();
+	
+		/* Save key for later */
+		await app.storage.post({
+			url: `/data/users/email/${req.body.email}`,
+			body: {authkey: key},
+			session: app.adminSession
+		});
+	
+		/* Data for recovery email */
+		const templateData = {
+			name: app.name,
+			key,
+			url: app.config.url
+		};
+	
+		/* Send authkey via email */
+		await app.notifications.sendNotification("lostpass", templateData, email);
 	}
 
-	/* Make sure key is > Date.now() */
-	var key = (Date.now() + 2 * 60 * 60 * 1000).toString(16);
-	key += this.app.utils.randString();
-
-	/* Save key for later */
-	await this.app.storage.post({
-		url: `/data/users/email/${req.body.email}`,
-		body: {authkey: key},
-		session: App.adminSession
-	});
-
-	/* Data for recovery email */
-	const templateData = {
-		name: this.app.name,
-		key,
-		url: this.app.config.url
-	};
-
-	/* Send authkey via email */
-	/* TODO: make subject customisable */
-	await this.app.notifications.sendNotification({
-		to: email,
-		subject: `Recover password for ${this.app.name}`,
-		html: this.app.notifications.forgotTemplateHTML(templateData)
-	});
-
+	/* Respond the same way whether or not we did anything */
 	/* If we need to redirect, let's redirect */
 	if (req.query.redirect) {
 		res.redirect(req.query.redirect);
