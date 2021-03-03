e4432a8d53ee789ae3cf7260a4ed5d41acfbe505
groenroos <oskari@groenroos.fi>
2021-03-03 01:14:00 +0000

    #26: Tests for Utils, move test data to _data

--- test/lib/Utils.test.js
+++ test/lib/Utils.test.js
@@ -1,5 +1,60 @@
 const test = require('ava');
+const fs = require('fs');
+const path = require('path');
+
+const SaplingError = require('../../lib/SaplingError');
 
 const Utils = require('../../lib/Utils');
 
-test.todo('tests Utils');
+
+test.before(t => {
+	t.context.utils = new Utils();
+
+	fs.chmodSync(path.join(__dirname, '../_data/inaccessible'), 0o100);
+});
+
+
+test('generates a random string', t => {
+	const random = t.context.utils.randString();
+
+	t.is(typeof random, 'string');
+	t.is(random.length, 11);
+
+	const random2 = t.context.utils.randString();
+
+	t.not(random, random2);
+});
+
+test('gets all files in an existing directory', t => {
+	const files = t.context.utils.getFiles(path.join(__dirname, '../_data/accessible'));
+
+	t.true(Array.isArray(files));
+	t.is(files.length, 2);
+	t.true(files[0].endsWith('bar/foo.txt'));
+	t.true(files[1].endsWith('file.txt'));
+});
+
+test('throws an error when getting files in a directory with no permissions', t => {
+	t.throws(() => {
+		t.context.utils.getFiles(path.join(__dirname, '../_data/inaccessible'));
+	}, {instanceOf: SaplingError});
+});
+
+test('throws an error when getting files in non-existent directory', t => {
+	t.throws(() => {
+		t.context.utils.getFiles(path.join(__dirname, '../_data/nonexistent'));
+	}, {instanceOf: SaplingError});
+});
+
+test('deep clones an object', t => {
+	const original = { foo: "bar", baz: [ 1, 2, 3 ], bar: { qux: true, quux: false } };
+	const clone = t.context.utils.deepClone(original);
+
+	t.deepEqual(original, clone);
+	t.not(original, clone);
+});
+
+
+test.after(t => {
+	fs.chmodSync(path.join(__dirname, '../_data/inaccessible'), 0o755);
+});
