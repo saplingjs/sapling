1e03f01ef3c97a78ef114b13c2b09f5bc6f39de0
groenroos <oskari@groenroos.fi>
2021-03-13 03:16:50 +0000

    #26: Tests for SaplingError

--- test/lib/SaplingError.test.js
+++ test/lib/SaplingError.test.js
@@ -2,4 +2,28 @@ const test = require('ava');
 
 const SaplingError = require('../../lib/SaplingError');
 
-test.todo('tests SaplingError');
+
+test('creates an error with a string', t => {
+	const error = new SaplingError('Error message');
+
+	t.is(typeof error.json, 'object');
+	t.is(error.json.errors.length, 1);
+	t.is(error.json.errors[0].title, 'Error message');
+});
+
+test('creates an error with an array', t => {
+	const error = new SaplingError(['Error 1', 'Error 2']);
+
+	t.is(typeof error.json, 'object');
+	t.is(error.json.errors.length, 2);
+	t.is(error.json.errors[0].title, 'Error 1');
+	t.is(error.json.errors[1].title, 'Error 2');
+});
+
+test('creates an error with an object', t => {
+	const error = new SaplingError({ title: 'Error message' });
+
+	t.is(typeof error.json, 'object');
+	t.is(error.json.errors.length, 1);
+	t.deepEqual(error.json.errors[0], { title: 'Error message' });
+});
